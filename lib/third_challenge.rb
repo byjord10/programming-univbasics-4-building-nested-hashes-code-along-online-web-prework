3029004d6c989d0bbc0a0e486115c91c6b2e9ec1
Brianna Jordan <bryvjordan9@gmail.com>
2019-08-21 00:48:38 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,7 +7,7 @@ def third_challenge
       :hero_friends => [
         "Benvolio" => {name: "Benvolio", age: "17", attitude: "worried"},
         "Mercutio" => {name: "Mercutio", age: "18", attitude: "hot-headed"}
-      ]
+      ]}
 
     },
     :capulet => {
